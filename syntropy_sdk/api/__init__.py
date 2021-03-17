4ac409b8a9a5ddec804feb088b165e1ab7a0924f
Andrius Mikonis <andrius@noia.network>
2021-03-17 14:16:52 +0200

    ADD: api key API.

--- syntropy_sdk/api/__init__.py
+++ syntropy_sdk/api/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 # import apis into api package
+from syntropy_sdk.api.api_keys_api import ApiKeysApi
 from syntropy_sdk.api.auth_api import AuthApi
 from syntropy_sdk.api.platform_api import PlatformApi
 
