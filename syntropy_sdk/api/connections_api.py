ccea71bb84cbde5402ef782077cf6c1f9a6b45d7
Andrius Mikonis <andrius@noia.network>
2021-09-15 16:20:14 +0300

    ADD: Deprecated hidden endpoint.

--- syntropy_sdk/api/connections_api.py
+++ syntropy_sdk/api/connections_api.py
@@ -139,6 +139,113 @@ class ConnectionsApi(object):
             collection_formats=collection_formats,
         )
 
+    def platform_connections_destroy_deprecated(self, body, **kwargs):  # noqa: E501
+        """Delete connections  # noqa: E501
+
+        Deletes connections between agents. Agent itself is not deleted.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.platform_connection_agents_destroy(body, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param list[AgentsObject] body: (required)
+        :return: None
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs["_return_http_data_only"] = True
+        if kwargs.get("async_req"):
+            return self.platform_connections_destroy_deprecated_with_http_info(
+                body, **kwargs
+            )  # noqa: E501
+        else:
+            (data) = self.platform_connections_destroy_deprecated_with_http_info(
+                body, **kwargs
+            )  # noqa: E501
+            return data
+
+    def platform_connections_destroy_deprecated_with_http_info(
+        self, body, **kwargs
+    ):  # noqa: E501
+        """Delete connections  # noqa: E501
+
+        Deletes connections between agents. Agent itself is not deleted.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.platform_connection_agents_destroy_with_http_info(body, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param list[AgentsObject] body: (required)
+        :return: None
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ["body"]  # noqa: E501
+        all_params.append("async_req")
+        all_params.append("_return_http_data_only")
+        all_params.append("_preload_content")
+        all_params.append("_request_timeout")
+
+        params = locals()
+        for key, val in six.iteritems(params["kwargs"]):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method platform_connection_agents_destroy" % key
+                )
+            params[key] = val
+        del params["kwargs"]
+        # verify the required parameter 'body' is set
+        if "body" not in params or params["body"] is None:
+            raise ValueError(
+                "Missing the required parameter `body` when calling `platform_connection_agents_destroy`"
+            )  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        if "body" in params:
+            body_params = params["body"]
+        # HTTP header `Content-Type`
+        header_params[
+            "Content-Type"
+        ] = self.api_client.select_header_content_type(  # noqa: E501
+            ["application/json"]
+        )  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ["jwt"]  # noqa: E501
+
+        return self.api_client.call_api(
+            "/api/platform/connections/remove",
+            "POST",
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type=None,  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get("async_req"),
+            _return_http_data_only=params.get("_return_http_data_only"),
+            _preload_content=params.get("_preload_content", True),
+            _request_timeout=params.get("_request_timeout"),
+            collection_formats=collection_formats,
+        )
+
     def platform_connection_create_mesh(self, body, **kwargs):  # noqa: E501
         """Create connections (mesh)  # noqa: E501
 
